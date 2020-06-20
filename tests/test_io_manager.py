19d8d0d8ee2bc81bfbf9c34bba15e6cb74d139a7
Kristoffer Nordstroem <kristoffer@nordstroem.ch>
2020-06-20 13:17:37 +0200

    + test for SW-AS-501

--- tests/test_io_manager.py
+++ tests/test_io_manager.py
@@ -1,11 +1,24 @@
 """Test IO handling"""
+import filecmp
+
 import fact.io_manager
-import fact.vcd_reader
 
 
-def test_basic_run(dummy_vcd_file):
-    """See if run terminates"""
-    app = fact.io_manager.InputOutputManager(
-        [dummy_vcd_file, dummy_vcd_file],
-        fact.vcd_reader.factory, 'test.vcd', False)
-    app.run()
+def test_simple_io(record_property, dummy_vcd_file):
+    """Feed it through unchanged and expect the same file"""
+    record_property("req", "SW-AS-501-44d4ac79")
+    ofile = 'test.vcd'
+    iom = fact.io_manager.InputOutputManager()
+    iom.merge_files([dummy_vcd_file], ofile)
+    assert filecmp.cmp(dummy_vcd_file, ofile)
+
+
+def test_read_write_engines(record_property, dummy_vcd_file):
+    """Write-back from read file, equal output"""
+    record_property("req", "SW-AS-501-44d4ac79")
+    ofile = 'test_writeback.vcd'
+    reader = fact.vcd_reader.factory(dummy_vcd_file)
+    writer = fact.vcd_writer.factory(ofile)
+
+    writer.process_source(reader)
+    assert filecmp.cmp(dummy_vcd_file, ofile)
