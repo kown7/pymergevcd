2bfecdd641821b42d945b8d79ff798ae38259df0
Kristoffer Nordstroem <kristoffer@nordstroem.ch>
2020-08-01 17:02:58 +0200

    Add merge_files command

--- src/fact/io_manager.py
+++ src/fact/io_manager.py
@@ -1,27 +1,14 @@
 """Manage input"""
-from typing import Callable, List
+from typing import List
 
 import fact.io_manager_interfaces as iomi
+import fact.vcd_reader
+import fact.vcd_writer
 
 
 class InputOutputManager:
     """Manages the entire data-flow"""
-    def __init__(self, in_files: List[str],
-                 factory: Callable[[str], iomi.AggregatorInterface],
-                 ofiles: List[str], enable_diff: bool):
-        self.readers = [factory(i) for i in in_files]
-
-    def run(self):
-        """Run configured readers and writers"""
-        elements = [list() for _ in range(len(self.readers))]
-        while self._all_readers_empty(elements):
-            pass
-
-    def _all_readers_empty(self, elements: List) -> bool:
-        all_empty = True
-        for i, reader in enumerate(self.readers):
-            element = reader.get_list()
-            elements[i] = element
-            if len(element) == 1 and isinstance(element[0], iomi.EndOfFile):
-                all_empty = False
-        return all_empty
+    def merge_files(self, in_files: List[str], ofile: str):
+        """Merge the files given in in_files to ofile"""
+        readers = [fact.vcd_reader.factory(i) for i in in_files]
+        writer = fact.vcd_writer.factory(ofile)
