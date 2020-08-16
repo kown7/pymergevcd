ef5e66e51813423338fcdc1c19439960885dbb45
Kristoffer Nordstroem <kristoffer@nordstroem.ch>
2020-08-16 22:38:06 +0200

    update references to AS

--- tests/test_io_manager.py
+++ tests/test_io_manager.py
@@ -10,7 +10,7 @@ def test_simple_io(record_property, dummy_vcd_file):
     The date and scope of the files will have changed accordingly.
 
     """
-    record_property('req', 'SW-AS-501-44d4ac79')
+    record_property('req', 'SW-AS-nnn-deadbeef')
     ofile = 'test_merge.vcd'
     iom = pymergevcd.io_manager.InputOutputManager()
     iom.merge_files([dummy_vcd_file], ofile)
@@ -20,7 +20,8 @@ def test_simple_io(record_property, dummy_vcd_file):
 
 def test_read_write_engines(record_property, dummy_vcd_file):
     """Write-back from read file, equal output"""
-    record_property('req', 'SW-AS-501-44d4ac79')
+    record_property('req', 'SW-AS-501-f8d68d11')
+    record_property('req', 'SW-AS-500-4c1a395a')
     ofile = 'test_writeback.vcd'
     reader = pymergevcd.vcd_reader.factory(dummy_vcd_file)
     writer = pymergevcd.vcd_writer.factory(ofile)
@@ -32,6 +33,7 @@ def test_read_write_engines(record_property, dummy_vcd_file):
 def test_merge_two_files(record_property, src_merge_file):
     """Merge two files with given return expected answer"""
     record_property('req', 'SW-AS-1-7d6734f3')
+    record_property('req', 'SW-AS-300-7b2b2d9d')
     ofile = 'merge_two_files.vcd'
     iom = pymergevcd.io_manager.InputOutputManager()
     iom.merge_files([src_merge_file[0], src_merge_file[1]],
--- tests/test_vcd_reader.py
+++ tests/test_vcd_reader.py
@@ -5,9 +5,19 @@ import pymergevcd.io_manager_interfaces as iomi
 import pymergevcd.vcd_reader
 
 
-def test_end_of_file(dummy_vcd_file):
-    """Test if data is read and it all ends with an End-Of-File"""
+def test_end_of_file(record_property, dummy_vcd_file):
+    """Test if data is read and it all ends with an End-Of-File
+
+    * SW-AS-200: assert `VcdReader` is an `AggregatorInterface`
+    * SW-AS-201: assert a list of `iomi.VcdElements` is returned
+    * Sw-AS-202: assert last element is `iomi.VcdElements`
+
+    """
+    record_property('req', 'SW-AS-200-b85e5b1b')
+    record_property('req', 'SW-AS-201-eb13fc0f')
+    record_property('req', 'SW-AS-202-35354cd8')
     vcdr = pymergevcd.vcd_reader.VcdReader(dummy_vcd_file)
+    assert isinstance(vcdr, iomi.AggregatorInterface)
     baseidx = 2
     entries = vcdr.get_list()
     assert isinstance(entries[0], iomi.VcdDate)
